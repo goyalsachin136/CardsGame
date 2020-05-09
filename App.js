c5cbac75aba6d540396c3edccffcde9e5d852cd2
sachin.goyal <sachin.goyal@dream11.com>
2020-05-09 19:30:38 +0530

    panna version 2.0 and get game data after calling generate player code

--- App.js
+++ App.js
@@ -294,7 +294,7 @@ export default function App() {
             <View>
                 <FlashMessage position="top" />
                 <Text style={styles.marginAround}>
-                    Welcome to 508 Version 2.0
+                    Welcome to PANNA Version 2.0
                 </Text>
                 {playerCode.length !== 0 ?
                     <View style={styles.inline}>
@@ -1197,8 +1197,15 @@ const generatePlayerCode = function (numericId, gameCode, playerNickName, onChan
                 subscribeAndBind(gameCode,json['message'], onChangeGameMessage,
                     onChangecardLeft1, onChangecardLeft2, onChangecardLeft3, onChangecardLeft4, onChangenickName1, onChangenickName2, onChangenickName3, onChangenickName4,
                     onChangesetsWon1, onChangesetsWon2, onChangesetsWon3, onChangesetsWon4, onChangesetsPointsWon1, onChangesetsPointsWon2, onChangesetsPointsWon3, onChangesetsPointsWon4,
-                    onChangePlayerToMove, onChangeTrumpDeclaredBy, onChangeCanGameBeStarted, onChangeTrumpCard, onChangeCurrentSet, onChangeCurrentSet1,  onChangeCurrentSet2,  onChangeCurrentSet3, onChangeCurrentSet4,  setRefreshing,
-                    onChangeHeartData, onChangeSpadeData, onChangeDiamondData, onChangeClubData, onChangeRelativePlayerToMove);
+                    onChangePlayerToMove, onChangeTrumpDeclaredBy, onChangeCanGameBeStarted, onChangeTrumpCard, onChangeCurrentSet, onChangeCurrentSet1,
+                    onChangeCurrentSet2, onChangeCurrentSet3, onChangeCurrentSet4,  setRefreshing, onChangeHeartData, onChangeSpadeData, onChangeDiamondData, onChangeClubData,
+                    onChangeRelativePlayerToMove);
+                getGameData(gameCode, json['message'], onChangeGameMessage,
+                    onChangecardLeft1, onChangecardLeft2, onChangecardLeft3, onChangecardLeft4, onChangenickName1, onChangenickName2, onChangenickName3, onChangenickName4,
+                    onChangesetsWon1, onChangesetsWon2, onChangesetsWon3, onChangesetsWon4, onChangesetsPointsWon1, onChangesetsPointsWon2, onChangesetsPointsWon3, onChangesetsPointsWon4,
+                    onChangePlayerToMove, onChangeTrumpDeclaredBy, onChangeCanGameBeStarted, onChangeTrumpCard, onChangeCurrentSet, onChangeCurrentSet1,
+                    onChangeCurrentSet2,  onChangeCurrentSet3, onChangeCurrentSet4,  setRefreshing, onChangeHeartData, onChangeSpadeData, onChangeDiamondData, onChangeClubData,
+                    onChangeRelativePlayerToMove)
             } else {
                 Alert.alert(json['message']);
             }
